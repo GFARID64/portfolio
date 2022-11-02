d5502a19567a6cc75685032af9f2bdcd30667ec1
GFARID64 <45872806+GFARID64@users.noreply.github.com>
2022-11-02 17:22:45 +0100

    mon premier commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiMacos,
+  SiCloudflare,
+  SiVercel,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMacos />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCloudflare />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+import gb from "../../Assets/Projects/gb.png";
+import sccr212 from "../../Assets/Projects/sccr212.png";
+import sccr11 from "../../Assets/Projects/11sccr.png";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={gb}
+              isBlog={false}
+              title="Gourmet Box"
+              description="Dynamic E-Commerce website dedicated to the employees of the business area. We developed the project with the “Express” Framework in Node JS. The DataBase processing is done via MongoDB and as for the code part, we used Bootstrap with HTML5 / CSS3 in HBS (handlebars) and Javascript."
+
+              demoLink="https://www.youtube.com/watch?v=Am2jrf3rKhw"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={sccr11}
+              isBlog={false}
+              title="Line-up Soccer212"
+              description="Creation of an application in react so that the user can have fun making his own team composition for the soccer212 site."
+
+              demoLink="https://soccer212projectcompo.pages.dev/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={sccr212}
+              isBlog={false}
+              title="Soccer212"
+              description="Creation of a sports information site specializing in Moroccan football. Site created with wordpress so that contributors can more easily write articles as they are used to doing with wordpress."
+
+              demoLink="https://soccer212.com/"
+            />
+          </Col>
+
+
+
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;

ce572c86fb8fe4a863302c4af35bd7cfd933b940
GFARID64 <45872806+GFARID64@users.noreply.github.com>
2022-11-03 10:32:11 +0100

    Update Home2.js
    delete text

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,8 +19,7 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming and I learn everyday.
               <br />
               <br />I am fluent in classics like
               <i>
@@ -28,16 +27,7 @@ function Home2() {
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
-                </b>
-              </i>
-              <br />
-              <br />
+             
               Whenever possible, I also apply my passion for developing products
               with <b className="purple">Node.js</b> and
               <i>

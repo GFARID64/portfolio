f422dfc790dcccaaa27b464d13327e07589b7e35
GFARID64 <45872806+GFARID64@users.noreply.github.com>
2024-06-19 12:44:51 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
+                I'M a
                 <strong className="main-name"> GHANDI FARID</strong>
               </h1>
 
